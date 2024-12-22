f0f86a9b3cf9978ed24e3c71b1e64d4223482252
Mahesh M Nair <43637836+maheshmnair@users.noreply.github.com>
2024-12-22 14:31:34 +0530

    moved the API fetch logic

--- script.js
+++ script.js
@@ -46,56 +46,8 @@ generateButton.addEventListener('click', async () => {
 });
     
 async function generateWords(word, language) {
-    const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
-    const apiUrl = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-pro:generateContent?key=' + apiKey;
-
-    const requestBody = {
-        "contents": [
-        {
-            "parts": [
-            {
-                "text": `You are an AI designed to translate words from English. Your mission is to translate the provided English word ${word} to it's correspoinding word in the ${language} language. Make sure to provide the exact translation. Generated Tranlated Word: (Print only one word and if there is any other then separate it by commas)\n`
-            }
-            ]
-        }
-        ],
-        "generationConfig": {
-        "temperature": 0.9,
-        "topK": 1,
-        "topP": 1,
-        "maxOutputTokens": 2048,
-        "stopSequences": []
-        },
-        "safetySettings": [
-        {
-            "category": "HARM_CATEGORY_HARASSMENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_HATE_SPEECH",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_SEXUALLY_EXPLICIT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        },
-        {
-            "category": "HARM_CATEGORY_DANGEROUS_CONTENT",
-            "threshold": "BLOCK_MEDIUM_AND_ABOVE"
-        }
-        ]
-    };
-
-    console.log(requestBody.contents[0].parts);
-    const response = await fetch(apiUrl, {
-        method: 'POST',
-        headers: {
-        'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(requestBody),
-    });
-
-    const responseData = await response.json();
-    const generatedNames = responseData.candidates[0].content.parts[0].text.split(',').map(name => name.trim());
-    return generatedNames;
+    const apiUrl = `https://backend.aint-mj.workers.dev/?word=${word}&language=${language}`;
+    const response = await fetch(apiUrl);
+    const data = await response.json();
+    return data || [];
 }
